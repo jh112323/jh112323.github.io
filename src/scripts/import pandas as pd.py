f57c2dac11acee1fcfaec9dce8e1afc8a84ca464
jh112323 <jonny.holmes@live.co.uk>
2025-04-28 23:13:52 +0100

    updates json source

--- src/scripts/import pandas as pd.py
+++ src/scripts/import pandas as pd.py
@@ -3,7 +3,7 @@ import os
 
 # Define the path to the Excel file and the output JSON file
 excel_file_path = '/Users/jonathan/Documents/PMAH/cave de camley.xlsx'
-json_file_path = '/Users/jonathan/Documents/PMAH/wine-list-web-project/src/data/wines.json'
+json_file_path = '/Users/jonathan/Documents/PMAH/jh112323.github.io/src/data/wines.json'
 
 def convert_excel_to_json(excel_path, json_path):
     # Load the Excel file
